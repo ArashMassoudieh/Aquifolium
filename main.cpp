bd1383ce69aa2ad19111e43a3b4fc45e4b543952
Unknown <arashmassoudieh@gmail.com>
2018-08-14 17:41:19 -0400

    MainFixed

--- main.cpp
+++ main.cpp
@@ -17,24 +17,24 @@ int main()
     B1.SetName("Reservoir1");
     S.AddBlock(B1);
 
-    S.block("myblock1")->SetVal("Storage",10);
-    S.block("myblock1")->SetVal("reservoir_power",2);
-    S.block("myblock1")->SetVal("reservoir_coeff",30);
+    S.block("Reservoir1")->SetVal("Storage",10);
+    S.block("Reservoir1")->SetVal("reservoir_power",2);
+    S.block("Reservoir1")->SetVal("reservoir_coeff",30);
 
     Block B2;
     B2.SetType("Reservoir");
     B2.SetName("Reservoir2");
     S.AddBlock(B2);
 
-    S.block("myblock2")->SetVal("Storage",5);
-    S.block("myblock2")->SetVal("reservoir_power",2);
-    S.block("myblock2")->SetVal("reservoir_coeff",30);
+    S.block("Reservoir2")->SetVal("Storage",5);
+    S.block("Reservoir2")->SetVal("reservoir_power",2);
+    S.block("Reservoir2")->SetVal("reservoir_coeff",30);
 //  cout<<S.block("myblock2")->GetVal("flow")<<endl;
 
     Link L;
     L.SetType("Reservoir_link");
     L.SetName("myLink");
-    S.AddLink(L,"myblock1","myblock2");
+    S.AddLink(L,"Reservoir1","Reservoir2");
     cout<<"flow = " << S.link("myLink")->CalcVal("flow")<<endl;
 	S.dt() = 0.01;
     S.Solve("Storage");
